509a83aea966ca2edaddb10a31c92f769a55ebbf
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-06-13 17:21:33 -0300

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
+require "ace-rails-ap"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  Article.create({ title: Faker::Lorem.word.upcase, content: Faker::Lorem.paragraph })
+end
